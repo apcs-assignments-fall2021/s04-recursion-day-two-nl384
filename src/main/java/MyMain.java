13786ae4c0137626374e7d8567799ebf956e5bc7
Nathan Lin <nlin@athenian.org>
2022-01-19 09:06:40 -0800

    asdf

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -46,8 +46,19 @@ public class MyMain {
     // countBigDigits(99999) => 5
     // countBigDigits(521931) => 2
     public static int countBigDigits(int x) {
-        // YOUR CODE HERE
-        return -1;
+//        // YOUR CODE HERE
+//        return -1;
+        // Base case
+        if (x == 0) {
+            return 0;
+        }
+        // Recursive call
+        else if (x % 10 >= 5){
+            return 1 + countBigDigits(x / 10);
+        }
+        else {
+            return countBigDigits(x / 10);
+        }
     }
 
     // Write a method that uses recursion to calculate whether
@@ -64,25 +75,51 @@ public class MyMain {
     // moreOddThanEven(99999) => true because 5 odd and 0 even
     // moreOddThanEven(521931) => true because 5 odd and 1 even
     public static boolean moreOddThanEven(int x) {
-        // YOUR CODE HERE
-        return false;
+//        // YOUR CODE HERE
+//        return false;
+        return moreOddThanEvenTR(x, 0, 0);
     }
 
-    // This might be helpful to get started:
-//    public static boolean moreOddThanEvenTR(...) {
+    public static boolean moreOddThanEvenTR(int x, int odd, int even) {
+        if (x == 0) {
+            return odd > even;
+        }
+        // Recursive call
+        else if (x % 2 == 0){
+            return moreOddThanEvenTR(x / 10, odd, even + 1);
+        }
+        else {
+            return moreOddThanEvenTR(x / 10, odd + 1, even);
+        }
+    }
 
-    // This method checks if a String is a palindrome
-    // (e.g. "racecar", "madam"), recursively
-    // Remember that a palindrome is a word that is the same forwards
-    // and backwards, so the first letter must match the last letter,
-    // the second letter must match the second to last letter, and so on
+    // Write a method that is given a String containing a single pair
+    // of matched parentheses containing some characters. This method
+    // should use recursion to "trim" off all the extra characters until
+    // only the parentheses and their contents remain
+    // You can assume that the String always contains this pair of parens
     // Examples:
-    // isPalindrome("racecar") => true
-    // isPalindrome("madam") => true
-    // isPalindrome("racecars") => false
-    public static boolean isPalindrome(String str) {
-        // YOUR CODE HERE
-        return false;
+    // parenTrim("xyz(abc)123") => "(abc)"
+    // parenTrim("x(hello)") => "(hello)"
+    // parenTrim("(xy)1") => "(xy)"
+    public static String parenTrim(String str) {
+//        // YOUR CODE HERE
+//        return "";
+        char c0 = str.charAt(0);
+        char c9 = str.charAt(str.length()-1);
+
+        if (c0== '(' && c9 == ')') {
+            return str;
+        }
+        else if (c0 != '(' && c9 != ')') {
+            return parenTrim(str.substring(1, str.length()-1));
+        }
+        else if (c0 != '(') {
+            return parenTrim(str.substring(1));
+        }
+        else {
+            return parenTrim(str.substring(0, str.length()-1));
+        }
     }
 
 
@@ -98,22 +135,38 @@ public class MyMain {
     // countBigDigits(99999) => 45
     // countBigDigits(521931) => 21
     public static int addDigits(int x) {
-        // YOUR CODE HERE
-        return -1;
+//        // YOUR CODE HERE
+//        return -1;
+        if (x < 10) {
+            return x;
+        }
+        else {
+            return (x % 10) + addDigits(x/10);
+        }
     }
 
-    // Write a method that is given a String containing a single pair
-    // of matched parentheses containing some characters. This method
-    // should use recursion to "trim" off all the extra characters until
-    // only the parentheses and their contents remain
-    // You can assume that the String always contains this pair of parens
+
+    // This method checks if a String is a palindrome
+    // (e.g. "racecar", "madam"), recursively
+    // Remember that a palindrome is a word that is the same forwards
+    // and backwards, so the first letter must match the last letter,
+    // the second letter must match the second to last letter, and so on
     // Examples:
-    // parenTrim("xyz(abc)123") => "(abc)"
-    // parenTrim("x(hello)") => "(hello)"
-    // parenTrim("(xy)1") => "(xy)"
-    public static String parenTrim(String str) {
-        // YOUR CODE HERE
-        return "";
+    // isPalindrome("racecar") => true
+    // isPalindrome("madam") => true
+    // isPalindrome("racecars") => false
+    public static boolean isPalindrome(String str) {
+//        // YOUR CODE HERE
+//        return false;
+        if (str.length() <= 1) {
+            return true;
+        }
+        else if(str.charAt(0) == str.charAt(str.length()-1)) {
+            return isPalindrome(str.substring(1, str.length()-1));
+        }
+        else {
+            return false;
+        }
     }
 
     // This method returns the orignal string reversed;
@@ -122,8 +175,14 @@ public class MyMain {
     // reverse("desserts") => stressed
     // reverse("racecar") => racecar
     public static String reverse(String str) {
-        // YOUR CODE HERE
-        return "";
+//        // YOUR CODE HERE
+//        return "";
+        if (str.length() == 0) {
+            return "";
+        }
+        else {
+            return reverse(str.substring(1)) + str.charAt(0);
+        }
     }
 
 
